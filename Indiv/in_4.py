c07b872d25ff88fc5ebe0a5dae046672218bc423
LissKovzogo <Silika0Clea@yandex.ru>
2025-10-11 13:16:32 +0300

    Задание повышенной сложности

--- Indiv/in_4.py
+++ Indiv/in_4.py
@@ -1,4 +1,20 @@
+import math
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
+
+    x = float(input("Введите x: "))
+    epsilon = 1e-10
+    gamma = 0.57721566490153286060
+
+    result = gamma + math.log(x)
+    n = 1
+    term = 1
+
+    while abs(term) > epsilon:
+        term = ((-1) ** n) * (x ** (2 * n)) / (2 * n * math.factorial(2 * n))
+        result += term
+        n += 1
+
+    print(f"Ci({x}) = {result}")
