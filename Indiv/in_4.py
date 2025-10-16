8a0ca4c7bc5d9a735e3b9c6659fbfbab84c5f49f
LissKovzogo <Silika0Clea@yandex.ru>
2025-10-16 21:17:37 +0300

    Исправление в отчёте

--- Indiv/in_4.py
+++ Indiv/in_4.py
@@ -5,16 +5,16 @@ import math
 if __name__ == "__main__":
 
     x = float(input("Введите x: "))
-    epsilon = 1e-10
+    eps = 1e-10
     gamma = 0.57721566490153286060
 
-    result = gamma + math.log(x)
+    result_f = gamma + math.log(x)
     n = 1
-    term = 1
-
-    while abs(term) > epsilon:
-        term = ((-1) ** n) * (x ** (2 * n)) / (2 * n * math.factorial(2 * n))
-        result += term
+    a = -(x**2)/4
+    result = a
+    while abs(a) > eps:
+        a *= ((-x**2)*(2*n))/((2*n+2)*(2*n+2)*(2*n+1))
+        result += a
         n += 1
 
-    print(f"Ci({x}) = {result}")
+    print(f"Ci({x}) = {result_f + result}")
